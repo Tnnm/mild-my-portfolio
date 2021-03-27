a0313323580c2efc047a0ed3ffcf5ce2739fc746
Mild <>
2021-03-28 01:19:38 +0700

    add page scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./style/app.scss";
+import "./assets/style/app.scss";
 import reportWebVitals from "./reportWebVitals";
 import FirstPage from "./pages/index";
 ReactDOM.render(
